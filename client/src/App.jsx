42185678144171f06cb53428cdc9c3fb5862f60a
mat-ng <matthewng122@gmail.com>
2022-04-12 22:41:28 -0400

    Added webhook to record button clicks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,26 @@ import React, { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 
 
-const App = () => {
+const socket = io()
 
-    useEffect(() => {
-        let socket = io()
-    }, [])
+const App = () => {
+    const [clicks, setClicks] = useState(0)
+
+    socket.on('new-clicks', arg => {
+        setClicks(arg)
+    })
 
     return (
-        <p>Hello World</p>
+        <div>
+            <p>{clicks}</p>
+            <button 
+                onClick={() => {
+                    setClicks(clicks + 1)
+                    socket.emit('clicks', clicks + 1)
+                }}>
+                Click here
+            </button>
+        </div>
     )
 }
 
--- server/src/server.js
+++ server/src/server.js
@@ -13,9 +13,14 @@ const app = express()
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 const io = socketIO(app)
-	.on('connection', (socket) => {
-		console.log('Client connected')
-		socket.on('disconnect', () => console.log('Client disconnected'))
-	})
+	.on('connection', socket => {
+		socket.on('clicks', (data) => {
+			io.emit('new-clicks', data)
+		})
 
-// setInterval(() => io.emit('time', new Date().toTimeString()), 1000)
+		console.log('Client connected')
+
+		socket.on('disconnect', () => {
+			console.log('Client disconnected')
+		})
+	})
