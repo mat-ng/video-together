2a7a411b2f1c985bfaca5c273bfdc8bcf2d89f19
mat-ng <matthewng122@gmail.com>
2022-04-16 18:45:58 -0400

    Adjusted margins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Video from './components/Video.jsx'
 const App = () => {
     return (
         <div>
-            <div style={{display: 'flex', justifyContent: 'center', paddingBottom: 30}}>
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: 20, paddingBottom: 25}}>
                 <img src={Banner} style={{height: 60}}/>
             </div>
             <Video/>
