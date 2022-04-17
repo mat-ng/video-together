bd99673bca975c65c26b2aa478dabe77b100a955
mat-ng <matthewng122@gmail.com>
2022-04-17 14:38:57 -0400

    Disabled mobile browsers

--- client/src/components/IsMobileBrowser.jsx
+++ client/src/components/IsMobileBrowser.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react'
 
 const IsMobileBrowser = () => {
     const [width, setWidth] = useState(window.innerWidth)
+    const [height, setHeight] = useState(window.innerHeight)
 
     const handleWindowSizeChange = () => {
             setWidth(window.innerWidth)
+            setHeight(window.innerHeight)
     }
 
     useEffect(() => {
@@ -14,7 +16,7 @@ const IsMobileBrowser = () => {
         }
     }, []);
 
-    return (width <= 768)
+    return (width > height ? width < 1190 : height < 1190)
 }
 
 export default IsMobileBrowser
