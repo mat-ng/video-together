ac4e10dcd86998a97113bdc6b77d67cfb6eca478
mat-ng <matthewng122@gmail.com>
2022-04-24 15:32:27 -0400

    Added favicon

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const express = require('express')
 const dotenv = require('dotenv').config({path: path.resolve(__dirname, '../../.env')})
+const favicon = require('serve-favicon')
 const socketIO = require('socket.io')
 
 const PORT = process.env.PORT || 3001
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 	.use(express.static(path.resolve(__dirname, '../../client/dist')))
+	.use(favicon(path.resolve(__dirname, '../../client/src/assets/favicon.ico')))
 	.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 const io = socketIO(app)
