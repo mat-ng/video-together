59d0db30e080a4899a58d9d4d11f0501f9af84c3
mat-ng <matthewng122@gmail.com>
2022-08-30 00:02:59 -0400

    Adjusted margins

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,13 +12,13 @@ const App = () => {
                 <img src={SiteBanner} style={{height: 60}}/>
             </div>
             
-            {/* { IsMobileBrowser() ? 
+            { IsMobileBrowser() ? 
                 <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <h1 style={{fontFamily: 'Trebuchet MS', fontSize: 22, marginLeft: 10, marginRight: 10, textAlign: 'center'}}>Sorry, this app is not supported on mobile or small screens.<br/><br/>If you are on a laptop/computer, please try zooming out.</h1>
                 </div>
-                :  */}
+                : 
                 <Video/> 
-            {/* } */}
+            }
         </div>
     )
 }
